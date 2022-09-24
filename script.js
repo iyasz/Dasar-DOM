34e24f4317bae0ffc93bb8f5b95ddf807ffb3e37
Yasz Avellia <zakamaragames@gmail.com>
2022-09-25 05:06:55 +0700

    menambah id lupa anjim

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // DOM SELECTION
-const anime = document.getElementById("judul");
-const btn = document.getElementById("btn");
-const doc1 = document.getElementById("doc");
-const doc2 = document.getElementById("doc2");
+// const anime = document.getElementById("judul");
+// const btn = document.getElementById("btn");
+// const doc1 = document.getElementById("doc");
+// const doc2 = document.getElementById("doc2");
 
-btn.addEventListener("click", () => {
-  anime.style.color = "red";
-  anime.style.transform = "scale(2)";
-  anime.style.transition = "2s";
-  anime.style.background = "blue";
-  anime.innerHTML = "SALAH";
-  doc2.innerHTML = "<h1>APABILA</h1>";
-});
+// btn.addEventListener("click", () => {
+//   anime.style.color = "red";
+//   anime.style.transform = "scale(2)";
+//   anime.style.transition = "2s";
+//   anime.style.background = "blue";
+//   anime.innerHTML = "SALAH";
+//   doc2.innerHTML = "<h1>APABILA</h1>";
+// });
 
-doc1.setAttribute("name", "controller");
+// doc1.setAttribute("name", "controller");
 // 2
-const a = document.getElementById("a");
 const elemen = document.createElement("h1");
 const textBaru = document.createTextNode("Kamu Keren");
 
 elemen.appendChild(textBaru);
+const a = document.getElementById("a");
 
 a.appendChild(elemen);
 
@@ -30,5 +30,7 @@ btn2.addEventListener("click", () => {
   const newText = document.createTextNode("Kamu Sugoi");
   newH.appendChild(newText);
 
-  a.appendChild(elemen);
+  const b = document.getElementById("b");
+
+  b.appendChild(newH);
 });
