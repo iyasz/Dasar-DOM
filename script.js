ca7c67e35f9cb52acffd2714047d147169985fcd
Yasz Avellia <zakamaragames@gmail.com>
2022-09-24 07:40:43 +0700

    style documnt

---
+++ script.js
@@ -0,0 +1,7 @@
+// DOM SELECTION
+const anime = document.getElementById("judul");
+const btn = document.getElementById("btn");
+
+document.addEventListener("click", () => {
+  anime.style.color = "red";
+});
