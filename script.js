309c25a70ce9cef556b5f370de099460b8714f8c
Yasz Avellia <zakamaragames@gmail.com>
2022-09-24 14:06:02 +0700

    lemeen style

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // DOM SELECTION
 const anime = document.getElementById("judul");
 const btn = document.getElementById("btn");
+const doc1 = document.getElementById("doc");
+const doc2 = document.getElementById("doc2");
 
 document.addEventListener("click", () => {
   anime.style.color = "red";
+  anime.style.transform = "scale(2)";
+  anime.style.transition = "2s";
+  anime.style.background = "blue";
+  anime.innerHTML = "SALAH";
+  doc2.innerHTML = "<h1>APABILA</h1>";
 });
