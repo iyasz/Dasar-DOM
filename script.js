b97452b2f80b55c42ec2bb5136d390dc53d9805c
Yasz Avellia <89033712+iyasz@users.noreply.github.com>
2022-09-30 20:42:05 +0700

    Hapus trnsision

--- script.js
+++ script.js
@@ -6,8 +6,6 @@
 
 // btn.addEventListener("click", () => {
 //   anime.style.color = "red";
-//   anime.style.transform = "scale(2)";
-//   anime.style.transition = "2s";
 //   anime.style.background = "blue";
 //   anime.innerHTML = "SALAH";
 //   doc2.innerHTML = "<h1>APABILA</h1>";
